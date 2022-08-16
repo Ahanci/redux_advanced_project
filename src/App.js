b58f4987bd3298f8d0d231dab8f640c07c0c49ee
Abdulkerim Hancıoglu <ahancimerve@gmail.com>
2022-08-16 14:29:22 +0300

    handled_sideEffects_with_useEffect_in_component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import { useEffect } from 'react';
+import { useEffect, Fragment } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { uiActions } from './components/store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
-  const showCart= useSelector((state)=>state.ui.cartIsVisible);
-  const cart=useSelector((state)=> state.cart);
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  //   useEffect(()=>{ // WITH TIS STRUCTURE WE USE UPDATED DATA FROM REDUX STORE AND KEEP OUR DATABASE UPDATED
+  // fetch('https://redux-cart-75518-default-rtdb.firebaseio.com/cart.json', {method: 'PUT', body: JSON.stringify(cart)}) // WITH THIS METHOD DATA OVERWRITE TO OLD DATA, BUT IF WE USED 'POST' METHOD OUR DATA WOULD ADDED TO BOTTOM OF OLD DATA
+  //   }, [cart]);
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+      const response = await fetch(
+        'https://redux-cart-75518-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    });
+  }, [cart, dispatch]);
   
-  useEffect(()=>{ // WITH TIS STRUCTURE WE USE UPDATED DATA FROM REDUX STORE AND KEEP OUR DATABASE UPDATED
-fetch('https://redux-cart-75518-default-rtdb.firebaseio.com/cart.json', {method: 'PUT', body: JSON.stringify(cart)}) // WITH THIS METHOD DATA OVERWRITE TO OLD DATA, BUT IF WE USED 'POST' METHOD OUR DATA WOULD ADDED TO BOTTOM OF OLD DATA
-  }, [cart]);
 
   return (
-    <Layout>
-     {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/components/store/ui-slice.js
+++ src/components/store/ui-slice.js
@@ -1,12 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 const uiSlice= createSlice({
     name:'ui', //MUST BE UNIQ NAME
-    initialState:{cartIsVisible:false}, // CAN BE A VARIABLE WHICH YOU CAN DEFINE BEFORE,
+    initialState:{cartIsVisible:false, notification:null}, // CAN BE A VARIABLE WHICH YOU CAN DEFINE BEFORE,
     reducers:{
         toggle(state){
             state.cartIsVisible=!state.cartIsVisible; //TOOLKIT TRANSFORMS THIS CODE TO IMMUTABLE CODE AND SO YOU WONT FACE A PROBLEM,
 
-        }
+        },
+        showNotification(state,action){
+            state.notification={status:action.payload.status, title:action.payload.title, message: action.payload.message}
+        } // added new state for managing error state.
     }
 });
 export const uiActions =uiSlice.actions;
