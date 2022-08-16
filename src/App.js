0c4203f588969148dc0e301b104024c12728da5a
Abdulkerim Hancıoglu <ahancimerve@gmail.com>
2022-08-16 12:09:55 +0300

    manage_async_structure_with_useEffect_in_component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -5,6 +6,12 @@ import { useSelector, useDispatch } from 'react-redux/es/exports';
 
 function App() {
   const showCart= useSelector((state)=>state.ui.cartIsVisible);
+  const cart=useSelector((state)=> state.cart);
+  
+  useEffect(()=>{ // WITH TIS STRUCTURE WE USE UPDATED DATA FROM REDUX STORE AND KEEP OUR DATABASE UPDATED
+fetch('https://redux-cart-75518-default-rtdb.firebaseio.com/cart.json', {method: 'PUT', body: JSON.stringify(cart)}) // WITH THIS METHOD DATA OVERWRITE TO OLD DATA, BUT IF WE USED 'POST' METHOD OUR DATA WOULD ADDED TO BOTTOM OF OLD DATA
+  }, [cart]);
+
   return (
     <Layout>
      {showCart && <Cart />}
